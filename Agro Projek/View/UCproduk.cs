7327483a66c68c81a1b2dd28cd048455a01f87f8
Grimpascal <armadalautmerah1993@gmail.com>
2025-06-04 11:20:42 +0700

    m

--- Agro Projek/View/UCproduk.cs
+++ Agro Projek/View/UCproduk.cs
@@ -17,6 +17,7 @@ namespace Agro_Projek.View
             InitializeComponent();
         }
 
+
         private void buttonDB_Click(object sender, EventArgs e)
         {
             var mainForm = this.Parent as panelBox;
